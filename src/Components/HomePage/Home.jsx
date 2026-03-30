d773acaa5c987c131b998d5302a7efd289232889
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:47:09 +0600

    home and registation components design

---
+++ src/Components/HomePage/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Ima Home Pages</h2>
+        </div>
+    );
+};
+
+export default Home;
--- src/Routes/Routex.jsx
+++ src/Routes/Routex.jsx
@@ -1,7 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
+import Home from "../Components/HomePage/Home";
+import Registation from "../Components/Registation/Registation";
 
 const router = createBrowserRouter([
-    {path:'/',Component:Root}
+    {path:'/',Component:Root,children:[
+        {index:true,Component:Home},
+        {path:'registation',Component:Registation},
+        {path:'registation',Component:''},
+        {path:'registation',Component:''},
+        {path:'registation',Component:''},
+        {path:'registation',Component:''},
+    ]}
 ])
 export default router;
