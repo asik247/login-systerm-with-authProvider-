b4717929e729b379a7426c9762b285da1dd9d17d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:50:19 +0600

    profile implement

--- src/Routes/Routex.jsx
+++ src/Routes/Routex.jsx
@@ -4,13 +4,14 @@ import Home from "../Components/HomePage/Home";
 import Registation from "../Components/Registation/Registation";
 import LogIn from "../Components/LogIn/LogIn";
 import Orders from "../Components/Orders/Orders";
+import Profile from "../Components/Profile/Profile";
 
 const router = createBrowserRouter([
     {path:'/',Component:Root,children:[
         {index:true,Component:Home},
         {path:'registation',Component:Registation},
         {path:'orders',Component:Orders},
-        {path:'registation',Component:''},
+        {path:'profile',Component:Profile},
         {path:'registation',Component:''},
         {path:'registation',Component:''},
     ]}
