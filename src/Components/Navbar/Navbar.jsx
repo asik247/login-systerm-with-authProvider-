e682a18d1c8c20d315e29932600e283340621b1f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:33:00 +0600

    google font added and nav components create

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='robotoFont'>
+            <h1 className='font-bold text-center'>Hello Ima Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
