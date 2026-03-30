0955c8bc918effe02505d6328b7dad43f34e2305
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:48:32 +0600

    login design

--- src/Routes/Routex.jsx
+++ src/Routes/Routex.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Home from "../Components/HomePage/Home";
 import Registation from "../Components/Registation/Registation";
+import LogIn from "../Components/LogIn/LogIn";
 
 const router = createBrowserRouter([
     {path:'/',Component:Root,children:[
         {index:true,Component:Home},
         {path:'registation',Component:Registation},
-        {path:'registation',Component:''},
+        {path:'login',Component:LogIn},
         {path:'registation',Component:''},
         {path:'registation',Component:''},
         {path:'registation',Component:''},
