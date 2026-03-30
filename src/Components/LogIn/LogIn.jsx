7ee3d74dbf522d64809ec476c4ed2a13ade765da
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:54:39 +0600

    LogInn form design

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -3,14 +3,10 @@ import React from 'react';
 const LogIn = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
+            <div className="hero  min-h-screen">
+  <div className="hero-content flex-col ">
+    <div className="text-center">
       <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
