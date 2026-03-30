45b3557bb71d51dd86dcbe6335df221aa3b6ffd8
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 23:56:42 +0600

    design almost compleate

--- src/Components/Registation/Registation.jsx
+++ src/Components/Registation/Registation.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
 const Registation = () => {
+  
     return (
         <div>
             <div className="hero min-h-screen">
                 <div className="hero-content flex-col ">
                     <div className="text-center">
                         <h1 className="text-5xl font-bold">Registation now!</h1>
-                       
+
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <fieldset className="fieldset">
-                                <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
-                                <button className="btn btn-neutral mt-4">Registation</button>
-                            </fieldset>
+                            <form>
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <button className="btn btn-neutral mt-4">Registation</button>
+                                </fieldset>
+                            </form>
                         </div>
                     </div>
                 </div>
