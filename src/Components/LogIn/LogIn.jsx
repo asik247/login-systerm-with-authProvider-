c5916b59eda6b4a116137d8459c3215d42c8fa8a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-31 22:42:23 +0600

    login loading showing

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -6,7 +6,7 @@ import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../../Firebase/firebase.init';
 
 const LogIn = () => {
-  const { logInUsers,loginWithGoogle } = useContext(AuthContext);
+  const { logInUsers,loginWithGoogle,loading } = useContext(AuthContext);
   const [emailValue, handleEmailChange] = useMyHook('');
   const [passwordValue, handlePasswordChange] = useMyHook('');
   // show user state here;
@@ -81,7 +81,7 @@ const LogIn = () => {
                   <input type="password" value={passwordValue} className="input" onChange={handlePasswordChange} placeholder="Password" />
                   {/* Password Rest code here */}
                   <div onClick={passwordReset}><a className="link link-hover">Forgot password?</a></div>
-                  <button className="btn btn-neutral mt-4">Login</button>
+                  <button disabled={loading} className="btn btn-neutral mt-4">{loading? 'logging....':'Login'}</button>
                   {/* Google */}
                   <button onClick={loginWithGoogleHandler} className="btn mt-4 mb-3 bg-white text-black border-[#e5e5e5]">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
