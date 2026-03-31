d052987b74ffd0cbc8c6bb9180f1137ac7d1cc67
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-31 09:31:58 +0600

    hook usign and registation from code update

--- src/Components/Registation/Registation.jsx
+++ src/Components/Registation/Registation.jsx
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Contexts/AuthContext';
+import useMyHook from '../../Hooks/useMyHook';
 
 const Registation = () => {
 // RegistationUsers Recive;
 const {registationUsers} = useContext(AuthContext);
-console.log(registationUsers);
+// custom hook code here;
+const [emailValue,handleEmailChange] = useMyHook('');
+const submitBtn = (e)=>{
+    e.preventDefault();
+    console.log(emailValue);
+}
     return (
         <div>
             <div className="hero min-h-screen">
@@ -15,10 +21,12 @@ console.log(registationUsers);
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={submitBtn}>
                                 <fieldset className="fieldset">
+                                    {/* Email Input Field */}
                                     <label className="label">Email</label>
-                                    <input type="email" className="input" placeholder="Email" />
+                                    <input type="email" value={emailValue} onChange={handleEmailChange} className="input" placeholder="Email" />
+                                    {/* Password Input Field */}
                                     <label className="label">Password</label>
                                     <input type="password" className="input" placeholder="Password" />
                                     <button className="btn btn-neutral mt-4">Registation</button>
