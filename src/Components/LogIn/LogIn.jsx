a8b9fbb297b83976b3fc287325552a4bb5ebdb60
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-31 15:57:15 +0600

    login code implement

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router';
+import useMyHook from '../../Hooks/useMyHook';
+import { AuthContext } from '../../Contexts/AuthContext';
 
 const LogIn = () => {
+  const {logInUsers} = useContext(AuthContext);
+  const [emailValue, handleEmailChange] = useMyHook('');
+  const [passwordValue, handlePasswordChange] = useMyHook('');
+  console.log(emailValue, passwordValue);
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    logInUsers(emailValue,passwordValue)
+    .then(res=>{
+      console.log(res.user);
+    }).catch(error=>{
+      console.log(error.message);
+    })
+
+
+
+  }
   return (
     <div>
       <div className="hero  min-h-screen">
@@ -11,17 +29,21 @@ const LogIn = () => {
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input type="password" className="input" placeholder="Password" />
-                <div><a className="link link-hover">Forgot password?</a></div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
-              <div>
-                New to our website ? please <NavLink className={'text-blue-600 font-bold underline text-sm'} to={'/registation'}> Registation</NavLink>
-              </div>
+              <form onSubmit={loginSubmit}>
+                <fieldset className="fieldset">
+                  {/* Email Input field */}
+                  <label className="label">Email</label>
+                  <input type="email" onChange={handleEmailChange} className="input" value={emailValue} placeholder="Email" />
+                  {/* Password Input field */}
+                  <label className="label">Password</label>
+                  <input type="password" value={passwordValue} className="input" onChange={handlePasswordChange} placeholder="Password" />
+                  <div><a className="link link-hover">Forgot password?</a></div>
+                  <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+                <div>
+                  New to our website ? please <NavLink className={'text-blue-600 font-bold underline text-sm'} to={'/registation'}> Registation</NavLink>
+                </div>
+              </form>
             </div>
           </div>
         </div>
--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
@@ -8,8 +8,13 @@ const AuthProvider = ({children}) => {
     const registationUsers = (email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password);
     }
+    // LogIn/SignIn code start here;
+    const logInUsers = (email,password)=>{
+       return signInWithEmailAndPassword(auth,email,password)
+    }
     const userInfo = {
        registationUsers,
+       logInUsers
     }
     return (
       <AuthContext value={userInfo}> 
